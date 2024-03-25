4c1e49e3460676909f9f2d6d1213d9df87b9954e
mishka49 <andreym3ch@gmail.com>
2024-03-25 16:44:00 +0300

    create logic

--- crypto_backend_app/updater/services.py
+++ crypto_backend_app/updater/services.py
@@ -1,54 +1,97 @@
-import json
+# import json
 
 import requests
 
-from coins import serializers
-from coins.models import Coin
-from coins.repositories import CoinRepository
-from coins.serializers import CoinSerializer
+# from coins import serializers
+# from coins.models import Coin
+# from coins.repositories import CoinRepository
+# from coins.serializers import CoinSerializer
 
 
-class UpdaterService:
-    @staticmethod
-    def update_coins(coins):
-        print(f"Coins: {coins}")
-        for coin in coins:
-            serializer = CoinSerializer(data=coin)
-            if serializer.is_valid():
-                print("API valided")
-                continue
+# class UpdaterService:
+#     @staticmethod
+#     def update_coins(coins):
+#         print(f"Coins: {coins}")
+#         for coin in coins:
+#             serializer = CoinSerializer(data=coin)
+#             if serializer.is_valid():
+#                 print("API valided")
+#                 continue
+#
+#             print("API None")
+#             CoinRepository.create_or_update(coin)
+#
+#
+# class ExternalApiHandler:
+#     @staticmethod
+#     def get_coins():
+#         request = requests.get('https://api.coinlore.net/api/tickers/')
+#
+#         for coin in request.json()['data']:
+#             print(coin)
+#             serializer = CoinSerializer(data=coin)
+#
+#             for field in Coin._meta.fields:
+#                 print('model', 'request')
+#                 print(field.value, coin[field.name])
+#
+#             if not serializer.is_valid():
+#                 print("API NONE")
+#                 return None
+#
+#         return request.json()
 
-            print("API None")
-            CoinRepository.create_or_update(coin)
+    # @staticmethod
+    # def get_coin():
+    #     pass
 
 
-class ExternalApiHandler:
-    @staticmethod
-    def get_coins():
-        request = requests.get('https://api.coinlore.net/api/tickers/')
+def get_info():
+    import json
+    import websocket
+    import pandas as pd
 
-        for coin in request.json()['data']:
-            print(coin)
-            serializer = CoinSerializer(data=coin)
+    assets = ['BTCUSDT']
 
-            for field in Coin._meta.fields:
-                print('model', 'request')
-                print(field.value, coin[field.name])
+    assets = [coin.lower() + '@kline_1m' for coin in assets]
+    assets = '/'.join(assets)
 
-            if not serializer.is_valid():
-                print("API NONE")
-                return None
- 
-        return request.json()
+    def on_message(mw, message):
+        message = json.loads(message)
+        print(message)
+
+    socket = "wss://stream.binance.com:9443/stream?streams=" + assets
+
+    ws = websocket.WebSocketApp(socket, on_message=on_message)
+
+    ws.run_forever()
 
 
+class APIService:
+    pass
 
 
-    @staticmethod
-    def get_coin():
+class CoinsUpdater:
+    pass
+
+
+class UpdaterFacadeService:
+    def __init__(self, update_frequency, coins_list, api_service=APIService(), updater=CoinsUpdater):
+        self.update_frequency = update_frequency
+        self.coins_list = coins_list
+        self.api_service = api_service
+        self.updater = updater
+
+    def run_stream(self):
+        pass
+
+    def stop_stream(self):
         pass
 
 
+
 if __name__ == '__main__':
-    coins = ExternalApiHandler.get_coins()
-    UpdaterService.update_coins(coins)
+    # coins = ExternalApiHandler.get_coins()
+    # UpdaterService.update_coins(coins)
+
+    get_info()
